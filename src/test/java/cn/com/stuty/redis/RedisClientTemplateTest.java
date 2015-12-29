fa32c35cd824aa6743cec4cfa1a8b8d1fbb785e2
liangzi4454 <1026163977@qq.com>
2015-12-29 22:10:54 +0800

    update code LHY

--- src/test/java/cn/com/stuty/redis/RedisClientTemplateTest.java
+++ src/test/java/cn/com/stuty/redis/RedisClientTemplateTest.java
@@ -3,14 +3,18 @@ package cn.com.stuty.redis;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 
 import cn.com.study.redis.RedisClientTemplate;
 
 public class RedisClientTemplateTest {
 	@Test
 	public void test() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/redis-datasource.xml");
+		XmlWebApplicationContext context = new XmlWebApplicationContext();
+		context.setConfigLocations(new String[] {"/WEB-INF/spring/redis/redis-datasource.xml"});
+//		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/redis-datasource.xml");
 		RedisClientTemplate redisClient = (RedisClientTemplate)context.getBean("redisClientTemplate");
+		
 		for(int i=0; i<10; i++) {
 			redisClient.set("a_b_c_"+i, "abc_"+i);
 		}
